fe8f07477533ad969905da678a81cb6c215914ea
Lieset Estampados <liesetestampados@gmail.com>
2026-03-05 08:02:46 -0300

    Arreglo tamaño de imagenes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 function consultar(producto){
 
-let mensaje = "Hola quiero consultar por " + producto
+let mensaje = "Hola quiero consultar por este articulo " + producto
 
-let url = "https://wa.me/549XXXXXXXXXX?text=" + encodeURIComponent(mensaje)
+let url = "https://wa.me/5492646237734 text=" + encodeURIComponent(mensaje)
 
 window.open(url)
 
